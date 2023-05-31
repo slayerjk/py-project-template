19e3add230cb9b8ba8cb5e3d36cf5848bd45ba71
slayerjk <slayer.jk@mail.ru>
2023-05-31 12:36:39 +0600

    Update project_new.py
    PEP8

--- project_new.py
+++ project_new.py
@@ -1,44 +1,37 @@
 #!/usr/bin/env python3
 
-### IMPORT PROJECTS PARTS
+# IMPORT PROJECTS PARTS
 from project_helper import count_estimated_time, files_rotate
 from project_common import appname, start_date_n_time, logging, logs_dir, logs_to_keep, app_log_name
 from project_mail_report import send_mail_option, send_mail_report
 
-
-####################
-### PROJECT DATA ###
-'''
+# PROJECT DATA
+"""
 Section for project's vars
-'''
+"""
 
-######################
-### MAIN FUNCTIONS ###
-######################        
-'''
+# MAIN FUNCTIONS
+"""
 Section for your app's various functions
-'''
+"""
 
-#####################
-### MAIN WORKFLOW ###
+# MAIN WORKFLOW
 
-### SCRIPT STARTED ALERT
+# SCRIPT STARTED ALERT
 logging.info(f'SCRIPT WORK STARTEDED: {appname}')
 logging.info(f'Script Starting Date&Time is: {str(start_date_n_time)}')
 logging.info('----------------------------\n')
 
-### RUN FUNCTIONS
+# RUN FUNCTIONS
 
+# POST-WORK PROCEDURES
 
-############################
-### POST-WORK PROCEDURES ###
-
-### FINISH JOBS
+# FINISH JOBS
 logging.info('#########################')
-logging.info('SUCCEEDED: Script job done!') 
+logging.info('SUCCEEDED: Script job done!')
 
 logging.info(count_estimated_time(start_date_n_time))
 logging.info('----------------------------')
 files_rotate(logs_dir, logs_to_keep, app_log_name)
 if send_mail_option == 'yes':
-    send_mail_report('report', app_log_name, start_date_n_time)
+    send_mail_report('report', app_log_name, start_date_n_time, app_log_name)
