5d10ebb65fbf7a003a12826839675fdd72e10d34
slayerjk <slayer.jk@mail.ru>
2023-09-22 14:44:22 +0600

    Update project_mailing.py
    Added auth option (with starttls)

--- project_mailing.py
+++ project_mailing.py
@@ -1,31 +1,41 @@
 """
-Sending emails(smtp): reports or simple sending
+Settings and functions for e-mail(smtp) reporting
 """
 
 from datetime import datetime
 from smtplib import SMTP
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from ssl import create_default_context
 
 
-# SIMPLE SEND MAIL FUNCTION
-def send_mail(mail_to, mail_from, smtp_server, smtp_port, mail_data):
-    message = MIMEMultipart()
-    message["From"] = mail_from
-    message["Subject"] = 'TEST MAILING SUBJECT'
-    message["To"] = mail_to
-    rcpt_to = mail_to
-    message.attach(MIMEText(mail_data, "html"))
-    with SMTP(smtp_server, smtp_port) as mail:
+# SIMPLE SEND EMAIL FUNCTION W/WO AUTH
+def send_mail(mail_to, mail_from, smtp_server, smtp_port, mail_data, login=None, password=None):
+    # Try to log in to server and send email
+    with SMTP(smtp_server, smtp_port) as server:
+        # DEBUG: 1 or 2(with timestamp)
+        # server.set_debuglevel(1)
+
+        # USE AUTH: STARTTLS IF LOGIN IS NOT NONE
+        if login and password:
+            context = create_default_context()
+            server.starttls(context=context)  # Secure the connection
+            server.login(login, password)
+        server.ehlo()  # Can be omitted
+        # Send email here
+        message = MIMEMultipart()
+        message["From"] = mail_from
+        message["Subject"] = 'TEST MAILING SUBJECT'
+        message["To"] = mail_to
+        rcpt_to = mail_to
+        message.attach(MIMEText(mail_data, "html"))
         data = message.as_string()
-        mail.ehlo()
-        mail.sendmail(mail_from, rcpt_to, data)
-        mail.quit()
+        server.sendmail(mail_from, rcpt_to, data)
 
 
-# EMAIL REPORT
+# EMAIL REPORT W/WO AUTH
 def send_mail_report(smtp_server, smtp_port, log_name, subj_date, main_log,
-                     from_addr, to_addr_list_admins=None, to_addr_list_users=None):
+                     from_addr, to_addr_list_admins=None, to_addr_list_users=None, login=None, password=None):
     """
     To send email report at.
     By default, at the end of the script only.
@@ -56,11 +66,19 @@ def send_mail_report(smtp_server, smtp_port, log_name, subj_date, main_log,
             report = log.read()
             message.attach(MIMEText(report, "plain"))
         try:
-            with SMTP(smtp_server, smtp_port) as mail:
+            with SMTP(smtp_server, smtp_port) as server:
+                # DEBUG: 1 or 2(with timestamp)
+                # server.set_debuglevel(1)
+
+                # USE AUTH: STARTTLS IF LOGIN IS NOT NONE
+                if login and password:
+                    context = create_default_context()
+                    server.starttls(context=context)  # Secure the connection
+                    server.login(login, password)
                 data = message.as_string()
-                mail.ehlo()
-                mail.sendmail(from_addr, rcpt_to, data)
-                mail.quit()
+                server.ehlo()
+                server.sendmail(from_addr, rcpt_to, data)
+                server.quit()
                 if type == 'error':
                     print(f'{datetime.now()} - INFO - SUCCEEDED: sending email error report\n')
                 else:
