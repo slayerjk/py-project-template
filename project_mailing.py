42edbacfd80f9bdb7125e4d825c7e3ff8628f14c
slayerjk <slayer.jk@mail.ru>
2023-09-28 15:25:00 +0600

    Update project_mailing.py

--- project_mailing.py
+++ project_mailing.py
@@ -34,57 +34,43 @@ def send_mail(mail_to, mail_from, smtp_server, smtp_port, mail_data, login=None,
 
 
 # EMAIL REPORT W/WO AUTH
-def send_mail_report(smtp_server, smtp_port, log_name, subj_date, main_log,
-                     from_addr, to_addr_list_admins=None, to_addr_list_users=None, login=None, password=None):
+def send_mail_report(appname, to_mail_list, mail_from, smtp_server, smtp_port, log_file, login=None, password=None,):
     """
     To send email report at.
     By default, at the end of the script only.
-    Args are:
-        - type: may be:
-            'error' - send whole log with error
-            'report' - send whole log after script done
-        - log_name - log file name(to send as report)
-        - subj_date - date format to insert into subject
-        - main_log - whole log data(if has one), to write status
+
+    Args:
+        appname: may be appname from project_static
+        to_mail_list: emails list, maybe mail_list_users/mail_list_admins from project_static
+        mail_from: may be smtp_from_addr from project_static
+        smtp_server: may be smtp_server from project_static
+        smtp_port: may be smtp_port from project_static
+        log_file: any file, maybe app_log_name from project_static
+        login: may be smtp_login from project_static
+        password: may be smtp_pass from project_static
     """
     message = MIMEMultipart()
-    message["From"] = from_addr
+    message["From"] = mail_from
 
-    with open(main_log, 'a'):
-        if type == 'error':
-            print(f'{datetime.now()} - INFO - STARTED: sending email error report')
-            message["Subject"] = f'appname - Script Error({subj_date})'
-            message["To"] = ', '.join(to_addr_list_admins)
-            rcpt_to = to_addr_list_admins
-        elif type == 'report':
-            print(f'{datetime.now()} - INFO - STARTED: sending email final report')
-            message["Subject"] = f'appname - Script Report({subj_date})'
-            message["To"] = ', '.join(to_addr_list_users)
-            rcpt_to = to_addr_list_users
+    with open(log_file, 'r') as log:
+        message["Subject"] = f'{appname} - Script Error({subj_date})'
+        message["To"] = ', '.join(to_mail_list)
+        rcpt_to = to_mail_list
+        report = log.read()
+        message.attach(MIMEText(report, "plain"))
+    try:
+        with SMTP(smtp_server, smtp_port) as server:
+            # DEBUG: 1 or 2(with timestamp)
+            # server.set_debuglevel(1)
 
-        with open(log_name, 'r') as log:
-            report = log.read()
-            message.attach(MIMEText(report, "plain"))
-        try:
-            with SMTP(smtp_server, smtp_port) as server:
-                # DEBUG: 1 or 2(with timestamp)
-                # server.set_debuglevel(1)
-
-                # USE AUTH: STARTTLS IF LOGIN IS NOT NONE
-                if login and password:
-                    context = create_default_context()
-                    server.starttls(context=context)  # Secure the connection
-                    server.login(login, password)
-                data = message.as_string()
-                server.ehlo()
-                server.sendmail(from_addr, rcpt_to, data)
-                server.quit()
-                if type == 'error':
-                    print(f'{datetime.now()} - INFO - SUCCEEDED: sending email error report\n')
-                else:
-                    print(f'{datetime.now()} - INFO - SUCCEEDED: sending email final report\n')
-        except Exception as e:
-            if type == 'error':
-                raise Exception(f'{datetime.now()} - WARNING - FAILED: sending email error report, moving on...\n{e}')
-            else:
-                raise Exception(f'{datetime.now()} - WARNING - FAILED: sending email final report, moving on...\n{e}')
+            # USE AUTH: STARTTLS IF LOGIN IS NOT NONE
+            if login and password:
+                context = create_default_context()
+                server.starttls(context=context)  # Secure the connection
+                server.login(login, password)
+            data = message.as_string()
+            server.ehlo()
+            server.sendmail(from_addr, rcpt_to, data)
+            server.quit()
+    except Exception as e:
+        raise Exception(f'{datetime.now()} - WARNING - FAILED: sending email report\n{e}')
